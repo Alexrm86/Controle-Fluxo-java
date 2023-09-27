e8dd4786b1643d02e13354461f4edf4f757499c1
Alex Resendende Machado <alex.magal@gmail.com>
2023-09-27 07:57:00 -0300

    atualização

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,10 +1,9 @@
 public class ResultadoEscolar {
   public static void main(String[] args) {
-     int nota=8;
-     if (nota>=7)
-     System.out.println("APROVADO");
-     else
-     System.out.println("REPROVADO");
+     int nota=6;
+     String Resultado= nota >=7? "APROVADO":nota>=5 && nota<7 ?"RECUPERAÇÃO": "REPROVADO";
+     System.out.println(Resultado);
+     
   }
   
 }
---
+++ src/SistemaDeMedida.java
@@ -0,0 +1,18 @@
+public class SistemaDeMedida {
+  public static void main(String[] args) {
+    String sigla="M";
+
+    if(sigla=="P")
+    System.out.println("PEQUENO");
+    else if (sigla=="M")
+      System.out.println("MÉDIO");
+    else if (sigla=="G")
+      System.out.println("GRANDE");
+    else
+      System.out.println("INDEFINIDO");
+
+
+
+  }
+  
+}
