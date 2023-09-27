7dd1bea2623e8cfaf90f194a7216158b36ebd551
Alex Resendende Machado <alex.magal@gmail.com>
2023-09-27 08:15:05 -0300

    swtch case

--- src/SistemaDeMedida.java
+++ src/SistemaDeMedida.java
@@ -2,16 +2,29 @@ public class SistemaDeMedida {
   public static void main(String[] args) {
     String sigla="M";
 
-    if(sigla=="P")
-    System.out.println("PEQUENO");
-    else if (sigla=="M")
-      System.out.println("MÉDIO");
-    else if (sigla=="G")
-      System.out.println("GRANDE");
-    else
-      System.out.println("INDEFINIDO");
+    switch(sigla){
+      case "P":{
+        System.out.println("PEQUENO");
+        break;
+      }
+      case "M":{
+        System.out.println("MÉDIO");
+        break;
+      }
+      
+        case "G":{
+        System.out.println("GRANDE");
+        break;  
+      } 
+  
+      default:{
+        System.out.println("INDEFINIDO");
+          
+      } 
+    
 
-
+    }
+  
 
   }
   
